92a2634fa809773d3079c97be362e2abfa4237d6
liuwenjing <15797707647@163.com>
2018-08-27 11:21:05 +0800

    修改按钮class值

--- src/components/userSetting/userSetting.vue
+++ src/components/userSetting/userSetting.vue
@@ -13,7 +13,7 @@
           <div class="search_btn_wrapper">
             <div class="search_btn red-btn" @click="queryBtn">查询</div>
             <div class="add_btn red-btn" @click="newUsers">新增</div>
-            <!--<div class="import_btn blue-btn" @click="importExcel">导入</div>-->
+            <!--<div class="import_btn red-btn" @click="importExcel">导入</div>-->
           </div>
         </div>
       </div>
