ba07a6897609bbb5d3b49b3bd32bb6917c4d3d9f
liuwenjing <15797707647@163.com>
2018-08-27 18:11:26 +0800

    登录成功默认进入发票库存查询页面

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -58,7 +58,7 @@
               Vue.prototype.$http.defaults.headers.common['x-access-token'] = getCookie('JD_token') || '';
               this.loginError = false;
               this.errorMessage = '';
-              this.$router.push('/');
+              this.$router.push('/billRepertory');
             }
           });
         }
